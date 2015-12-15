39a3f2144b1ade53b04704ab8473397b8eca3b19
migao <migao@rdamicro.com>
2015-12-15 17:22:22 +0800

    HeapSort.cpp.txt_to_HeapSort.cpp.

--- HeapSort.cpp
+++ HeapSort.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
-3include<algorithm>
+#include<algorithm>
 
 using namespace std;
 
 void Adjustdown(int heap[],int index,int size){
-	if(index<=size/2){
+	if(index*2<=size){
 		int min=index,lchild=index*2,rchild=index*2+1;
 		if(lchild<=size&&heap[lchild]<heap[min])
 			min=lchild;
@@ -17,25 +17,25 @@ void Adjustdown(int heap[],int index,int size){
 	}
 }
 
-void BuildHeap(int heap[],int size)){
-	for(int i=size/2;i>1;i--){
+void BuildHeap(int heap[],int size){
+	for(int i=size/2;i>=1;i--){
 		Adjustdown(heap,i,size);
 	}
 }
 
 void HeapSort(int heap[],int size){
 	BuildHeap(heap,size);
-	for(,size>2,size--){
+	for(;size>1;){
 		swap(heap[1],heap[size]);
-		Adjustdown(heap,1,size-1);
+		Adjustdown(heap,1,--size);
 	}
 }
 
-int main(int arg,char *argv[]){
-	int heap[]={0,3,6,2,1,2,6,9,4,1};
+int main(){
+	int heap[]={0,1,2,3,4,5,6,7,8,9};
 	HeapSort(heap,9);
-	for(int i=1,i<10,i++)
-		cout<<"heap[i] ";
+	for(int i=1;i<10;i++)
+		cout<<heap[i]<<" ";
 	cout<<endl;
 	return 0;
 }
